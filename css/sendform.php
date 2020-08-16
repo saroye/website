665f30fc6e381bf6c0bf5fb90bf11e3f4f1b1d33
mohit <saroyemohit@gmail.com>
2020-08-16 08:46:03 -0700

    added new features

---
+++ css/sendform.php
@@ -0,0 +1,20 @@
+<?php
+if(isset($_POST['Submit'])){
+    $name=$_POST['name'];
+    $name=$_POST['Phone'];
+    $name=$_POST['Email'];
+    $name=$_POST['Message'];
+
+    $to='saroyemohit@gmail.com';
+    $subject='Message from Website/';
+    $message= "Name: " .$name."\n"."Phone: "."\n". "Message is as follows: "."\n\n".$Message;
+    $headers="From: ".$email;
+    if(mail($to, $subject, $message, $headers)){
+
+        echo "<h1> Thank you for your interest!"." ".$name.", I will reply you as soon as I recieve this message! <h1>";
+    }
+    else{
+        echo "Error: 404";
+    }
+}
+?>
